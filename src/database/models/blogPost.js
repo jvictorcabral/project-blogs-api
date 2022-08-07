3a365d3e14d533a893800a31c3de68846f2ed28c
Joao <joaovictorcgjvcg@gmail.com>
2022-08-07 16:40:49 -0400

    entregando a model BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,30 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  }
+
+  return BlogPost;
+}
+
+module.exports = BlogPost;
