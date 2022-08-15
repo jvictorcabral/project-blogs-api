7fb7831881731e99f5181a6d4a56325493fe4379
Joao <joaovictorcgjvcg@gmail.com>
2022-08-15 19:55:56 -0400

    fix: corrigindo requisito 12

--- src/server.js
+++ src/server.js
@@ -11,8 +11,7 @@ const validateToken = require('./middlewares/validateToken');
 const validateUser = require('./middlewares/validateUser');
 const validateUpdatePost = require('./middlewares/validateUpdatePost');
 const verifyUser = require('./middlewares/verifyUser');
-const validateNonexistentPost = require('./middlewares/validateReq16');
-// const validateReq16 = require('./middlewares/validateReq16');
+const validateNonexistentPost = require('./middlewares/validateNonexistentPost');
 
 // não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
@@ -34,7 +33,6 @@ app.get('/post/:id', validateToken, postController.getById);
 app.put('/post/:id', validateToken, verifyUser, validateUpdatePost, postController.updatePost);
 app.delete('/post/:id', validateToken, validateNonexistentPost, 
 verifyUser, postController.deletePost);
-// app.delete('/post/:id', validateToken, validateReq16, postController.deletePost);
 app.delete('/user/me', validateToken, userController.deleteUser);
 
 app.listen(port, () => console.log('ouvindo porta', port));
